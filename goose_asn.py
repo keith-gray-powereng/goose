f0b9f9e225ca5c96c7a8b7205503aeb9a0db569b
Keith Gray <idahogray@gmail.com>
2016-09-23 15:02:42 -0400

    Continued building the GOOSE asn1 definition and testing it against the captured goose message in goose.txt

--- goose_asn.py
+++ goose_asn.py
@@ -1,7 +1,123 @@
 # coding: utf-8
 
 from pyasn1.codec.ber import encoder
-from pyasn1.type import univ, namedtype, tag, char
+from pyasn1.type import char, namedtype, tag, univ, useful
+
+
+class FloatingPoint(univ.OctetString):
+    pass
+
+
+class Data(univ.Choice):
+    componentType = namedtype.NamedTypes(
+        namedtype.NamedType('boolean', univ.Boolean().subtype(
+            implicitTag=tag.Tag(
+                tag.tagClassContext,
+                tag.tagFormatSimple,
+                3
+            )
+        )),
+        namedtype.NamedType('bit-string', univ.BitString().subtype(
+            implicitTag=tag.Tag(
+                tag.tagClassContext,
+                tag.tagFormatSimple,
+                4
+            )
+        )),
+        namedtype.NamedType('integer', univ.Integer().subtype(
+            implicitTag=tag.Tag(
+                tag.tagClassContext,
+                tag.tagFormatSimple,
+                5
+            )
+        )),
+        # TODO: Unsigned Should Never be Negative
+        namedtype.NamedType('unsigned', univ.Integer().subtype(
+            implicitTag=tag.Tag(
+                tag.tagClassContext,
+                tag.tagFormatSimple,
+                6
+            )
+        )),
+        namedtype.NamedType('floating-point', FloatingPoint().subtype(
+            implicitTag=tag.Tag(
+                tag.tagClassContext,
+                tag.tagFormatSimple,
+                7
+            )
+        )),
+        namedtype.NamedType('octet-string', univ.OctetString().subtype(
+            implicitTag=tag.Tag(
+                tag.tagClassContext,
+                tag.tagFormatSimple,
+                9
+            )
+        )),
+        namedtype.NamedType('visible-string', char.VisibleString().subtype(
+            implicitTag=tag.Tag(
+                tag.tagClassContext,
+                tag.tagFormatSimple,
+                10
+            )
+        )),
+        namedtype.NamedType('generalized-time', useful.GeneralizedTime().subtype(
+            implicitTag=tag.Tag(
+                tag.tagClassContext,
+                tag.tagFormatSimple,
+                11
+            )
+        )),
+        # TODO: Binary Time should be of TimeOfDay type with size of 4 or 6
+        namedtype.NamedType('binary-time', univ.OctetString().subtype(
+            implicitTag=tag.Tag(
+                tag.tagClassContext,
+                tag.tagFormatSimple,
+                12
+            )
+        )),
+        # TODO: BCD Should Never be Negative
+        namedtype.NamedType('bcd', univ.Integer().subtype(
+            implicitTag=tag.Tag(
+                tag.tagClassContext,
+                tag.tagFormatSimple,
+                13
+            )
+        )),
+        namedtype.NamedType('booleanArray', univ.BitString().subtype(
+            implicitTag=tag.Tag(
+                tag.tagClassContext,
+                tag.tagFormatSimple,
+                14
+            )
+        )),
+        namedtype.NamedType('objId', univ.ObjectIdentifier().subtype(
+            implicitTag=tag.Tag(
+                tag.tagClassContext,
+                tag.tagFormatSimple,
+                15
+            )
+        )),
+        # TODO: MMS String should be its own type
+        namedtype.NamedType('mMSString', char.UTF8String().subtype(
+            implicitTag=tag.Tag(
+                tag.tagClassContext,
+                tag.tagFormatSimple,
+                16
+            )
+        )),
+        # TODO: UTC Time should be its own type
+        namedtype.NamedType('utc-time', univ.OctetString().subtype(
+            implicitTag=tag.Tag(
+                tag.tagClassContext,
+                tag.tagFormatSimple,
+                17
+            )
+        ))
+    )
+
+
+class AllData(univ.SequenceOf):
+    componentType = Data()
 
 
 class IECGoosePDU(univ.Sequence):
@@ -86,7 +202,7 @@ class IECGoosePDU(univ.Sequence):
                 )
             )
         ),
-        namedtype.DefaultedNamedType(
+        namedtype.NamedType(
             'confRev',
             univ.Integer().subtype(
                 implicitTag=tag.Tag(
@@ -106,13 +222,23 @@ class IECGoosePDU(univ.Sequence):
                 )
             )
         ),
-        namedtype.DefaultedNamedType(
+        namedtype.NamedType(
             'numDatSetEntries',
             univ.Integer().subtype(
                 implicitTag=tag.Tag(
                     tag.tagClassContext,
                     tag.tagFormatSimple,
                     10
+                )
+            )
+        ),
+        namedtype.NamedType(
+            'allData',
+            AllData().subtype(
+                implicitTag=tag.Tag(
+                    tag.tagClassContext,
+                    tag.tagFormatConstructed,
+                    11
                 )
             )
         ),
@@ -131,13 +257,44 @@ class IECGoosePDU(univ.Sequence):
 
 if __name__ == '__main__':
     g = IECGoosePDU()
-    g.setComponentByName('gocbRef', 'ABCDE')
-    g.setComponentByName('timeAllowedtoLive', 2)
-    g.setComponentByName('datSet', 'Dset_TT')
+    g.setComponentByName('gocbRef', 'PDC02_11_700G_G1CFG/LLN0$GO$GooseDset_BF')
+    g.setComponentByName('timeAllowedtoLive', 2000)
+    g.setComponentByName('datSet', 'PDC02_11_700G_G1CFG/LN0$Dset_BF')
+    g.setComponentByName('goID', '11_700G_G1_Dset_BF')
     g.setComponentByName('t', 'some time')
-    g.setComponentByName('stNum', 0)
-    g.setComponentByName('sqNum', 0)
-    g.setComponentByName('confRev', 0)
-    g.setComponentByName('numDatSetEntries', 0)
+    g.setComponentByName('stNum', 5)
+    g.setComponentByName('sqNum', 1757)
+    g.setComponentByName('test', False)
+    g.setComponentByName('confRev', 3)
+    g.setComponentByName('numDatSetEntries', 6)
+    d = AllData().subtype(
+        implicitTag=tag.Tag(
+            tag.tagClassContext,
+            tag.tagFormatConstructed,
+            11
+        )
+    )
+    d1 = Data()
+    d1.setComponentByName('boolean', False)
+    d2 = Data()
+    d2.setComponentByName('bit-string', "'0000'B")
+    d3 = Data()
+    d3.setComponentByName('utc-time', '0000')
+    d4 = Data()
+    d4.setComponentByName('boolean', False)
+    d5 = Data()
+    d5.setComponentByName('bit-string', "'0000'B")
+    d6 = Data()
+    d6.setComponentByName('utc-time', '0000')
+    d.setComponentByPosition(0, d1)
+    d.setComponentByPosition(1, d2)
+    d.setComponentByPosition(2, d3)
+    d.setComponentByPosition(3, d4)
+    d.setComponentByPosition(4, d5)
+    d.setComponentByPosition(5, d6)
+    print(d)
+    print(d.prettyPrint())
+    g.setComponentByName('allData', d)
+    print(g.prettyPrint())
 
     print(encoder.encode(g))
